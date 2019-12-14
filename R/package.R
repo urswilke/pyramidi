aa604a4e85e714d9649180677a9d08b7de437331
Urs Wilke <urs.wilke@gmail.com>
2019-12-14 19:04:41 +0100

    deal with reticulate

--- R/package.R
+++ R/package.R
@@ -3,8 +3,14 @@
 # https://rstudio.github.io/reticulate/articles/package.html
 
 mido <- NULL
+miditapyr <- NULL
+# reticulate::use_python('/home/chief/anaconda3/bin/python3.7')
 
-.onLoad <- function(mido, pkgname) {
-  # use superassignment to update global reference to mido
+.onLoad <- function(libname, pkgname) {
+  # use superassignment to update global reference to mido & miditapyr
+  # miditapyr <<- reticulate::import("miditapyr", delay_load = TRUE)
+
   mido <<- reticulate::import("mido", delay_load = TRUE)
+  reticulate::use_python('/home/chief/anaconda3/bin/python3.7')
+  miditapyr <<- reticulate::import("miditapyr")
 }
