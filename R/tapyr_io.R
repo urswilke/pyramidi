36a5ce6ed082a739da2f0575c13018e85a181e40
Urs Wilke <urs.wilke@gmail.com>
2019-12-14 21:11:37 +0100

    first working version of save data frames to midi file

--- R/tapyr_io.R
+++ R/tapyr_io.R
@@ -23,5 +23,36 @@ mido_midi <- function(mid_file) {
 #'
 midi_df <- function(mid_file) {
   mido_midi(mid_file) %>%
-    miditapyr$mido_midi_df()
+    miditapyr$mido_midi_df() %>%
+    purrr::map(tibble::as_tibble) %>%
+    purrr::set_names(c("df_meta", "df_notes", "ticks_per_beat"))
+}
+
+#' Write
+#'
+#' @param df_meta
+#' @param df_notes
+#' @param ticks_per_beat
+#'
+#' @return
+#' @export
+#'
+#' @examples
+df2midi <- function(df_meta,
+                    df_notes,
+                    ticks_per_beat,
+                    file) {
+  miditapyr$df_2_midi(df_meta %>%
+                        dplyr::mutate(time = as.integer(time)),
+                      df_notes %>%
+                        dplyr::mutate(time = as.integer(time)),
+                      ticks_per_beat %>%
+                        dplyr::pull(),
+                      file)
+  # Create mido MidiFile object:
+  # mf <- miditapyr$df2mido_midifile(df_meta,
+  #                                  df_notes,
+  #                                  ticks_per_beat %>%
+  #                                    dplyr::pull())
+  # mf$save(file)
 }
