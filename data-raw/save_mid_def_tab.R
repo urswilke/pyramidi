b2745a61acc949b7c82b156791804b1a3ee63c4f
Urs Wile <urs.wilke@gmail.com>
2020-10-05 09:00:00 +0200

    midi table

--- data-raw/save_mid_def_tab.R
+++ data-raw/save_mid_def_tab.R
@@ -1,3 +1,7 @@
 reticulate::source_python("~/pyrmusic/python/py/py_funs.py")
-midi_defs <- get_midi_pitch_note_table() %>% dplyr::select(-note)
-usethis::use_data(midi_defs)
+midi_defs <- get_midi_pitch_note_table() %>%
+  dplyr::select(-note)%>%
+  dplyr::select(note = pitch, note_name = nameWithOctave_unique) %>%
+  dplyr::mutate(note_name = forcats::as_factor(note_name)) %>%
+  tibble::as_tibble()
+usethis::use_data(midi_defs, overwrite = TRUE)
