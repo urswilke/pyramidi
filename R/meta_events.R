953abce271cdc971a097a9159adc2fc0f25ddadf
Urs Wile <urs.wilke@gmail.com>
2020-10-07 18:31:52 +0200

    meta events doc etc

--- R/meta_events.R
+++ R/meta_events.R
@@ -1,11 +1,17 @@
 #' Tabulate time signature
 #'
-#' @param df_meta
+#' @param df_meta data.frame containing midi meta event data resulting of miditapyr$mido_midi_df()
 #'
 #' @return
-#' @export
 #'
 #' @examples
+#' library(zeallot)
+#' mt <- reticulate::import("miditapyr")
+#' mido <- reticulate::import("mido")
+#' mid_file <- system.file("extdata", "test_midi_file.mid", package = "pyramidi")
+#' mido$MidiFile(mid_file) %>%
+#'   mt$mido_midi_df()  %->% c(df_meta, df_notes, ticks_per_beat)
+#' tab_time_sig(df_meta)
 tab_time_sig <- function(df_meta) {
   df_meta %>%
     tibble::as_tibble() %>%
@@ -15,12 +21,18 @@ tab_time_sig <- function(df_meta) {
 
 #' Get tempo
 #'
-#' @param df_meta
+#' @param df_meta data.frame containing midi meta event data resulting of miditapyr$mido_midi_df()
 #'
 #' @return
-#' @export
 #'
 #' @examples
+#' library(zeallot)
+#' mt <- reticulate::import("miditapyr")
+#' mido <- reticulate::import("mido")
+#' mid_file <- system.file("extdata", "test_midi_file.mid", package = "pyramidi")
+#' mido$MidiFile(mid_file) %>%
+#'   mt$mido_midi_df()  %->% c(df_meta, df_notes, ticks_per_beat)
+#' get_tempo(df_meta)
 get_tempo <- function(df_meta) {
   df_meta %>%
     tibble::as_tibble() %>%
@@ -30,6 +42,21 @@ get_tempo <- function(df_meta) {
 get_ticks2second_scale <- function(df_meta, ticks_per_beat) {
   get_tempo(df_meta) * 1e-6 / ticks_per_beat
 }
+#' Get beats per minute
+#'
+#' @param df_meta  data.frame containing midi meta event data resulting of miditapyr$mido_midi_df()
+#'
+#' @return integer of beats per minute
+#' @export
+#'
+#' @examples
+#' library(zeallot)
+#' mt <- reticulate::import("miditapyr")
+#' mido <- reticulate::import("mido")
+#' mid_file <- system.file("extdata", "test_midi_file.mid", package = "pyramidi")
+#' mido$MidiFile(mid_file) %>%
+#'   mt$mido_midi_df()  %->% c(df_meta, df_notes, ticks_per_beat)
+#' get_bpm(df_meta)
 get_bpm <- function(df_meta) {
   60 * 1e6 / get_tempo(df_meta)
 }
