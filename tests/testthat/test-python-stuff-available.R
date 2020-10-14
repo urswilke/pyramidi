b3b8d041ab9d3aedf21648f3356ccd09ba8b4d70
Urs Wile <urs.wilke@gmail.com>
2020-10-14 23:52:16 +0200

    remove test

--- tests/testthat/test-python-stuff-available.R
+++
@@ -1,5 +0,0 @@
-test_that("python modules available", {
-  # testthat::expect_true(reticulate::py_available())
-  testthat::expect_true(reticulate::py_module_available("mido"))
-  testthat::expect_true(reticulate::py_module_available("miditapyr"))
-})
