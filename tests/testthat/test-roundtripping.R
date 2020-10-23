a54b44324239c134e3dbb54ab43c0be7f9e690c9
Urs Wilke <urs.wilke@gmail.com>
2020-10-23 15:09:42 +0200

    skip test on travis

--- tests/testthat/test-roundtripping.R
+++ tests/testthat/test-roundtripping.R
@@ -1,4 +1,9 @@
 test_that("applying mt$compact_df() and then mt$tidy_df() is roundtripping to the original dataframe", {
+  # reticulate fails on travis
+  # (error:
+  # "Attributemodule 'miditapyr' has no attribute 'midi_to_df'
+  # but it should have)
+  skip_on_travis()
   content <- system.file("example_files", "Bass_sample.mid", package="tuneR")
   mido <- reticulate::import("mido")
   mt <- reticulate::import("miditapyr")
