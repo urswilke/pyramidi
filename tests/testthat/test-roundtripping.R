b10d3226c13a1dbe2feac69731a5197cb5070392
Urs Wilke <urs.wilke@gmail.com>
2020-10-31 22:15:04 +0100

    new testthat version started to throw an error, so the "pandas.index" attribute was removed from the resulting data.frames

--- tests/testthat/test-roundtripping.R
+++ tests/testthat/test-roundtripping.R
@@ -8,6 +8,8 @@ test_that("applying mt$compact_df() and then mt$tidy_df() is roundtripping to th
   mido <- reticulate::import("mido")
   mt <- reticulate::import("miditapyr")
   a <- mido$MidiFile(content) %>% mt$midi_to_df() %>% mt$tidy_df()
+  attr(a, "pandas.index") <- NULL
   b <- a %>% mt$compact_df() %>% mt$tidy_df()
+  attr(b, "pandas.index") <- NULL
   expect_equal(a, b)
 })
