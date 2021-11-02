83da929b91661de5b37324df5eafaedf5acfcb5d
Urs Wilke <urs.wilke@gmail.com>
2021-11-02 22:34:04 +0100

    try to make tests run on gh actions

---
+++ R/load_python_pkgs.R
@@ -0,0 +1,14 @@
+# global reference to scipy (will be initialized in .onLoad)
+#' @export
+miditapyr <- NULL
+
+# global reference to scipy (will be initialized in .onLoad)
+#' @export
+mido <- NULL
+
+.onLoad <- function(libname, pkgname) {
+  # use superassignment to update global reference to miditapyr
+  miditapyr <<- reticulate::import("miditapyr", delay_load = TRUE)
+  # use superassignment to update global reference to mido
+  mido <<- reticulate::import("mido", delay_load = TRUE)
+}
--- tests/testthat/test-roundtripping.R
+++ tests/testthat/test-roundtripping.R
@@ -1,15 +1,16 @@
-test_that("applying mt$compact_df() and then mt$tidy_df() is roundtripping to the original dataframe", {
+# reticulate::use_python("/home/chief/anaconda3/bin/python")
+test_that("applying miditapyr$compact_df() and then miditapyr$tidy_df() is roundtripping to the original dataframe", {
   # reticulate fails on travis
   # (error:
   # "Attributemodule 'miditapyr' has no attribute 'midi_to_df'
   # but it should have)
   # skip_on_ci()
   content <- system.file("example_files", "Bass_sample.mid", package="tuneR")
-  mido <- reticulate::import("mido")
-  mt <- reticulate::import("miditapyr")
-  a <- mido$MidiFile(content) %>% mt$midi_to_df() %>% mt$tidy_df()
+  # mido <- reticulate::import("mido")
+  # miditapyr <- reticulate::import("miditapyr")
+  a <- mido$MidiFile(content) %>% miditapyr$midi_to_df() %>% miditapyr$tidy_df()
   attr(a, "pandas.index") <- NULL
-  b <- a %>% mt$compact_df() %>% mt$tidy_df()
+  b <- a %>% miditapyr$compact_df() %>% miditapyr$tidy_df()
   attr(b, "pandas.index") <- NULL
   expect_equal(a, b)
 })
