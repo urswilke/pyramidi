5535c8301b91f6398cedeb2239b25083a8d148dc
Urs Wilke <urs.wilke@gmail.com>
2021-11-03 12:12:45 +0100

    next 1

--- tests/testthat/test-roundtripping.R
+++ tests/testthat/test-roundtripping.R
@@ -1,10 +1,17 @@
-# reticulate::use_python("/home/chief/anaconda3/bin/python")
+reticulate::use_python("/home/chief/anaconda3/bin/python")
+skip_if_no_miditapyr <- function() {
+  have_miditapyr <- reticulate::py_module_available("miditapyr")
+  if (!have_miditapyr)
+    skip("miditapyr not available for testing")
+}
+
 test_that("applying miditapyr$compact_df() and then miditapyr$tidy_df() is roundtripping to the original dataframe", {
   # reticulate fails on travis
   # (error:
   # "Attributemodule 'miditapyr' has no attribute 'midi_to_df'
   # but it should have)
   # skip_on_ci()
+  skip_if_no_miditapyr()
   content <- system.file("example_files", "Bass_sample.mid", package="tuneR")
   # mido <- reticulate::import("mido")
   # miditapyr <- reticulate::import("miditapyr")
