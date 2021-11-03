4e0722a1822465aa2a6df2768c1d81b815cbfd9c
Urs Wilke <urs.wilke@gmail.com>
2021-11-03 16:08:48 +0100

    add tests for meta events ]

---
+++ tests/testthat/_snaps/meta_events.md
@@ -0,0 +1,10 @@
+# meta events work
+
+    Code
+      tab_time_sig(df_meta)
+    Output
+      # A tibble: 1 x 4
+        numerator denominator clocks_per_click notated_32nd_notes_per_beat
+            <dbl>       <dbl>            <dbl>                       <dbl>
+      1         4           4               24                           8
+
---
+++ tests/testthat/test-meta_events.R
@@ -0,0 +1,13 @@
+mid_file_str <- system.file("extdata", "test_midi_file.mid", package = "pyramidi")
+mido_mid_file <- mido$MidiFile(mid_file_str)
+dfc <- miditapyr$midi_to_df(mido_mid_file)
+ticks_per_beat = mido_mid_file$ticks_per_beat
+df <- dfc %>%
+  miditapyr$tidy_df()
+miditapyr$split_df(df)  %->% c(df_meta, df_notes)
+
+test_that("meta events work", {
+  testthat::expect_snapshot(tab_time_sig(df_meta))
+  expect_equal(get_tempo(df_meta), 666666)
+  expect_equal(round(get_bpm(df_meta), 2), 90)
+})
