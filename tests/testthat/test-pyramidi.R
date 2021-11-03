3dc7b75d8de9b25184b221ee92799660a60089d8
Urs Wilke <urs.wilke@gmail.com>
2021-11-03 16:40:23 +0100

    skip tests on ci

--- tests/testthat/test-pyramidi.R
+++ tests/testthat/test-pyramidi.R
@@ -1,15 +1,16 @@
-mid_file_str <- system.file("extdata", "test_midi_file.mid", package = "pyramidi")
-mido_mid_file <- mido$MidiFile(mid_file_str)
-dfc <- miditapyr$midi_to_df(mido_mid_file)
-ticks_per_beat = mido_mid_file$ticks_per_beat
-df <- dfc %>%
-  miditapyr$tidy_df()
-miditapyr$split_df(df)  %->% c(df_meta, df_notes)
-
-dfm <- tab_measures(df, ticks_per_beat)
-dfw <- dfm %>% widen_events()
 
 test_that("meta events work", {
+  skip_if_no_miditapyr()
+  mid_file_str <- system.file("extdata", "test_midi_file.mid", package = "pyramidi")
+  mido_mid_file <- mido$MidiFile(mid_file_str)
+  dfc <- miditapyr$midi_to_df(mido_mid_file)
+  ticks_per_beat = mido_mid_file$ticks_per_beat
+  df <- dfc %>%
+    miditapyr$tidy_df()
+  miditapyr$split_df(df)  %->% c(df_meta, df_notes)
+
+  dfm <- tab_measures(df, ticks_per_beat)
+  dfw <- dfm %>% widen_events()
   testthat::expect_snapshot(tab_time_sig(df_meta))
   testthat::expect_snapshot(dfm)
   testthat::expect_snapshot(dfw)
