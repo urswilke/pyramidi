9c5c155a9741f3c64563559e9dbef18bf5a7e374
Urs Wilke <urs.wilke@gmail.com>
2021-11-10 14:53:37 +0100

    avoid "Using an external vector in selections is ambiguous. i Use `all_of(values_from_vector)` instead of `values_from_vector` to silence this message."

--- R/widen.R
+++ R/widen.R
@@ -21,7 +21,7 @@ widen_events <- function(df_measures) {
   df_measures %>%
     # dplyr::select(c("i_track", "channel", "type", "m", "b", "t", "ticks", "time", "note", "velocity", "i_note")) %>%
     # dplyr::select(c("i_track", "name", "channel", "type", "m", "b", "t", "ticks", "time", "note", "velocity", "i_note")) %>%
-    tidyr::pivot_wider(names_from = c("type"), values_from = values_from_vector) %>%
+    tidyr::pivot_wider(names_from = c("type"), values_from = !!values_from_vector) %>%
     # as.data.frame() %>%
     dplyr::arrange(.data$i_track, .data$b_note_on)
 }
