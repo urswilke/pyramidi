7b62c16404b88ef3ab5b2355cf0d6bc8ccf9de39
Urs Wilke <urs.wilke@gmail.com>
2021-11-24 17:42:59 +0100

    add tests for empty MidiFramer objects

--- tests/testthat/test-r6midi.R
+++ tests/testthat/test-r6midi.R
@@ -11,3 +11,16 @@ test_that("MidiFramer seems to work", {
   ) %>%
     purrr::map(head, 20))
 })
+
+test_that("empty MidiFramer seems to work", {
+  mfr <- MidiFramer$new()
+  mfr_attrs <- c("midi_frame_mod", "df_notes_wide", "df_not_notes",
+    "df_meta", "df_notes_long", "dfm") %>%
+    purrr::set_names() %>%
+    purrr::map(~`[[`(mfr, .x))
+  mfr_attrs %>% purrr::map(expect_null)
+
+  expect_error(mfr$mf$midi_frame_raw)
+  expect_error(mfr$mf$midi_frame_unnested$df)
+  expect_error(mfr$mf$midi_frame_nested$df)
+})
