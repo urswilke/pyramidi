b64bf06456d3aff8f22bb6be6cd6be9daf18a398
urswilke <urs.wilke@gmail.com>
2024-02-15 23:05:23 +0100

    clean stochastic stuff from snapshot test

--- tests/testthat/test-midi_frames.R
+++ tests/testthat/test-midi_frames.R
@@ -2,7 +2,8 @@ midi_file_string <- system.file("extdata", "test_midi_file.mid", package = "pyra
 mfr <- r_midi_frames(midi_file_string)
 
 test_that("print of r_midi_frames() correct", {
-  expect_snapshot(mfr)
+  expect_snapshot(mfr,
+                  transform = \(x) stringr::str_replace(x, "0x[0-9a-f]{12}", "<stochastic term :)>"))
   expect_snapshot(list(
     mfr$mf$midi_frame_raw,
     mfr$mf$midi_frame_unnested$df,
